2f32af2def4af7d6cb473f1710bf64833bd04bf1
IgorNSMD <insanmartind@gmail.com>
2023-05-18 17:03:04 -0400

    P2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var queryDatabase = require('../config/testmysql.js')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express MySQL P1' });
+  res.render('index', { title: 'Express MySQL P2' });
 });
 
 module.exports = router;
