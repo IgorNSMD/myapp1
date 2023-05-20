0eedbc426750d33a746a058719372d0e315922cb
IgorNSMD <insanmartind@gmail.com>
2023-05-19 21:23:50 -0400

    P6

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var Task = require('../models/task')
+
 var router = express.Router();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express MySQL P4' });
+router.get('/', async function(req, res, next) {
+  const task = await Task.findAll({})
+  console.log(task)
+  res.render('index', { title: 'Express MySQL P6' });
 });
 
 module.exports = router;
