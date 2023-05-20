cb4588d6a494ed978012ce2157f0676803919a08
IgorNSMD <insanmartind@gmail.com>
2023-05-19 21:30:40 -0400

    P7

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var Task = require('../models/task')
+// var Task = require('../models/task')
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const task = await Task.findAll({})
-  console.log(task)
-  res.render('index', { title: 'Express MySQL P6' });
+  // const task = await Task.findAll({})
+  //console.log(task)
+  res.render('index', { title: 'Express MySQL P7' });
 });
 
 module.exports = router;
