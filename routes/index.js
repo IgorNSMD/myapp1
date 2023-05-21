591b5fe398a616f20d9f3723ebb756c8f3684cee
IgorNSMD <insanmartind@gmail.com>
2023-05-21 12:38:59 -0400

    ,,

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const task = await Task.findAll()
-  //console.log(task)
-  res.render('index', { title: 'Express MySQL P14' });
+  try {
+    const task = await Task.findAll()
+    //console.log(task)
+    res.render('index', { title: 'Express MySQL P15' });
+  } catch (error) {
+    console.log(error)
+  }
+
 });
 
 module.exports = router;
