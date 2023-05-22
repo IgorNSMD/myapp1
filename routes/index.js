bd499d36996ae2a78571bf36a947f376799736bb
IgorNSMD <insanmartind@gmail.com>
2023-05-22 11:23:25 -0400

    P17

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const task = await Task.findAll()
-    //console.log(task)
-    res.render('index', { title: 'Express MySQL P16' });
+    const task = await Task.findAll({
+      where: { taskName: "tarea 2" }
+   })
+    console.log(task)
+    res.render('index', { title: 'Express MySQL P17' });
   } catch (error) {
     console.log(error)
   }
